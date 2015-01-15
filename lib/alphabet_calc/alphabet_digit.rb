102f0f825a6324b3fb33d5d829e9f0281e74f332
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2015-01-15 21:32:28 +0700

    Make digit convert number back to alphabet

--- lib/alphabet_calc/alphabet_digit.rb
+++ lib/alphabet_calc/alphabet_digit.rb
@@ -1,12 +1,22 @@
 module AlphabetCalc
   class AlphabetDigit
 
-    def initialize(ch)
-      @ch = ch
+    def initialize(input)
+      if input =~ /\A\d+\z/
+        @num = input.to_i
+        @ch = (@num + 10).to_s(36)
+      else
+        @ch = input
+        @num = @ch.to_i(36) - 10
+      end
     end
 
     def to_int
-      @ch.to_i(36) - 10
+      @num
+    end
+
+    def to_str
+      @ch
     end
 
   end
--- spec/alphabet_calc/alphabet_digit_spec.rb
+++ spec/alphabet_calc/alphabet_digit_spec.rb
@@ -2,12 +2,27 @@ require 'spec_helper'
 require 'alphabet_calc/alphabet_digit'
 
 describe AlphabetCalc::AlphabetDigit do
-  it 'return correct value for "a"' do
-    a = AlphabetCalc::AlphabetDigit.new('a')
-    expect(a.to_int).to eq 0
+
+  describe 'initialize from alphabet' do
+    it 'return correct value for "a"' do
+      a = AlphabetCalc::AlphabetDigit.new('a')
+      expect(a.to_int).to eq 0
+    end
+    it 'return correct value for "z"' do
+      z = AlphabetCalc::AlphabetDigit.new('z')
+      expect(z.to_int).to eq 25
+    end
   end
-  it 'return correct value for "z"' do
-    z = AlphabetCalc::AlphabetDigit.new('z')
-    expect(z.to_int).to eq 25
+
+  describe 'initialize from number' do
+    it 'return correct alphabet for 0' do
+      a = AlphabetCalc::AlphabetDigit.new('0')
+      expect(a.to_str).to eq 'a'
+    end
+    it 'return correct alphabet for 25' do
+      z = AlphabetCalc::AlphabetDigit.new('25')
+      expect(z.to_str).to eq 'z'
+    end
   end
+
 end
